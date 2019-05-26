11d33a88d15b698304ac2ab5c0776dcc47108fbd
ArkadiCSGO <arkadioffi@gmail.com>
2019-05-26 20:48:37 +0200

    Add files via upload

---
+++ esx_bankerjob/config.lua
@@ -0,0 +1,16 @@
+Config                      = {}
+Config.DrawDistance         = 100.0
+Config.EnablePlayerManagement = true -- If set to true you need esx_realestateagentjob
+Config.BankSavingPercentage = 2.5
+Config.Locale = 'fr'
+
+Config.Zones = {
+
+  BankActions = {
+    Pos   = { x = 260.130, y = 204.308, z = 109.287 },
+    Size  = { x = 1.5, y = 1.5, z = 1.0 },
+    Color = { r = 102, g = 102, b = 204 },
+    Type  = 1
+  }
+
+}
